2770745a6c98fac02c637ac17e0c0fdccb3a3f2b
Md Shahbaz Alam <sa0418600@gmail.com>
2024-10-30 18:41:52 +0530

    changing browser router to hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
@@ -10,56 +10,41 @@ import NotFound from "./pages/NotFound";
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <div className="h-screen w-screen flex flex-col">
-          <Navbar loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
-          <Home />
-        </div>
-      ),
-    },
-    {
-      path: "/login",
-      element: (
-        <div>
-          <Navbar loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
-          <Login setLoggedIn={setLoggedIn} />
-        </div>
-      ),
-    },
-    {
-      path: "/signup",
-      element: (
-        <div>
-          <Navbar loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
-          <Signup setLoggedIn={setLoggedIn} />
-        </div>
-      ),
-    },
-    {
-      path: "/dashboard",
-      element: (
-        <div className="h-screen w-screen flex flex-col">
-          <Navbar loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
-          <Dashboard />
-        </div>
-      ),
-    },
-    {
-      path: "*",
-      element: (
-        <div>
-          <NotFound />
-        </div>
-      ),
-    },
-  ]);
-
   return (
     <div className="w-screen min-h-screen bg-richBlack-900 flex flex-col">
-      <RouterProvider router={router} />
+      <HashRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="h-screen w-screen flex flex-col">
+                <Navbar loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+                <Home />
+              </div>
+            }
+          />
+          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} />} />
+          <Route
+            path="/signup"
+            element={
+              <div>
+                <Navbar loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+                <Signup setLoggedIn={setLoggedIn} />
+              </div>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <div className="h-screen w-screen flex flex-col">
+                <Navbar loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+                <Dashboard />
+              </div>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 };
