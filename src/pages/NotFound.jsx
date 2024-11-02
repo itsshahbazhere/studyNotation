d0cf298939848f6d381284dcbc7fc404550648e8
Md Shahbaz Alam <sa0418600@gmail.com>
2024-11-02 23:04:48 +0530

    creating error-404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,9 +3,12 @@ import { NavLink } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div className='flex flex-col'>
-      <p>Error 404</p>
-      <NavLink to="/" className="text-blue-500">Go To HomePage</NavLink>
+    <div className='flex flex-col bg-white h-screen w-screen justify-center items-center select-none'>
+      <div className='relative flex flex-col justify-center items-center'>
+        <h1 className='text-8xl font-thin tracking-wide'>OOPS!</h1>
+        <p className='bg-white p-1 text-xs absolute bottom-1.5 text-center'>404 - THE PAGE CAN'T BE FOUND</p>
+      </div>
+      <NavLink to="/" className="text-white text-xs mt-5 uppercase bg-orange-500 px-2 py-1 rounded-sm">Go To HomePage</NavLink>
     </div>
   )
 }
